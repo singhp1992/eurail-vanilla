a11651ac4e0b49b59096e701997ae4e057a57daf
Preeti <singhp1992@gmail.com>
2019-06-14 13:24:56 +0200

    redo

--- functionality.js
+++ functionality.js
@@ -1,22 +1,19 @@
+// Given information
+
 var configJson = {
-    "title": "Contact List", "uri":"https://api.randomuser.me/",
+    "title": "Contact List", 
+    "uri":"https://api.randomuser.me/",
     "numberCards": 120,
     "tabs": ["a", "b", "c", "d", "e", "f", "g","h",
     "i", "j", "k","l","m","n","o","p","q","r","s","t",
     "u","v","w","x","y","z"]
 }
 
-function tabSwitch(new_tab, new_content) {
+console.log(configJson.tabs)
 
-    document.getElementById('content_1').style.display = 'none';
-    document.getElementById('content_2').style.display = 'none';
-    document.getElementById('content_3').style.display = 'none';
-    document.getElementById(new_content).style.display = 'block';
-
-
-    document.getElementById('tab_1').className = '';
-    document.getElementById('tab_2').className = '';
-    document.getElementById('tab_3').className = '';
-    document.getElementById(new_tab).className = 'active';
-
-}
+$(document).ready(function () {
+    var supercarObject = { "brand": "Lamborghini", "model": "Huracan", "origin": "Italy" };
+    $.each(supercarObject, function (key, value) {
+        $("#result").append(key + ": " + value + '<br>');
+    });
+});
