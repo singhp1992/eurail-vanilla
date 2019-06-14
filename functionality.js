997d2ab4ab7e6dcaa78938177d6af885cb9a4875
Preeti <singhp1992@gmail.com>
2019-06-14 13:56:49 +0200

    used loop to assign ids!!

--- functionality.js
+++ functionality.js
@@ -15,17 +15,20 @@ $(document).ready(function () {
         "7": "g", "8": "h", "9": "i", "10": "j", "11": "k", "12": "l", "13": "m", "14": "n",
         "15": "o", "16": "p", "17": "q", "18": "r", "19": "s", "20": "t", "21": "u", "22": "v",
         "23": "w", "24": "x", "25": "y", "26": "z"
-};
+    };
     $.each(tabs, function (key, value) {
         $("#result").append('<li>' + '<a>' + value + '</a>' + '</li>');
     });
-    $.each(tabs, function (key, value) {
-        $("li").attr('id', key);
-    });
+    // $.each(tabs, function (key, value) {
+    //     $("li").attr('id', key);
+    //     console.log(key)
+    // });
 });
 
 // Adding id to li 
-// $(document).ready(function() {
-   
-// })
+$(document).ready(function() {
+    $("ul li").each(function (i) {
+        $(this).attr("id", i);
+    });
+})
 
