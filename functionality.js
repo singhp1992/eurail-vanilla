3871ed9f0b9ab0e8d2be3d6e7f41d7c95beebb4a
Preeti <singhp1992@gmail.com>
2019-06-14 13:51:03 +0200

    added an id but not the right one

--- functionality.js
+++ functionality.js
@@ -11,14 +11,21 @@ var configJson = {
 
 
 $(document).ready(function () {
-    var supercarObject = { "1": "a", "2": "b", "3": "c", "4": "d", "5": "e", "6": "f",
+    var tabs = { "1": "a", "2": "b", "3": "c", "4": "d", "5": "e", "6": "f",
         "7": "g", "8": "h", "9": "i", "10": "j", "11": "k", "12": "l", "13": "m", "14": "n",
         "15": "o", "16": "p", "17": "q", "18": "r", "19": "s", "20": "t", "21": "u", "22": "v",
         "23": "w", "24": "x", "25": "y", "26": "z"
 };
-    $.each(supercarObject, function (key, value) {
+    $.each(tabs, function (key, value) {
         $("#result").append('<li>' + '<a>' + value + '</a>' + '</li>');
+    });
+    $.each(tabs, function (key, value) {
+        $("li").attr('id', key);
     });
 });
 
-// Adding elements to li 
+// Adding id to li 
+// $(document).ready(function() {
+   
+// })
+
