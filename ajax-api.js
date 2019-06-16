4639c17fe795a9f656ffde7fe95ef3e52ee924aa
Preeti <singhp1992@gmail.com>
2019-06-16 12:45:16 +0200

    only bs showing!

--- ajax-api.js
+++ ajax-api.js
@@ -25,24 +25,27 @@
 // }); 
 // $(document).on('click', "nav li.active", function () {
 
-// $(window).load(function () {
-$(document).on('click', "nav li.active", function () {
+
+// $(document).on('click', "nav li.active", function () {
+$(window).load(function () {
     var $a = $('#a')
     $.ajax({
-        url: 'https://randomuser.me/api/?results=8',
+        url: 'https://api.randomuser.me/?inc=gender,name,nat,email&results=100&nat=us,dk,fr,gb',
         dataType: 'json',
         success: function (data) {
             console.log(data.results)
 
             $.each(data.results, function (i, order) {
                 console.log(order.name.last[0])
-                $($a).append('<li class="more-info">' + order.name.last + ', ' + order.name.first +'</li>');
-                // if ('b' === order.name.last[0]) {
-                //     console.log('working!')
-                // }
-                // else {
-                //     console.log('try again')
-                // }
+                if ('b' === order.name.last[0]) {
+                    console.log('working!')
+                    // $('#a').text(order.name.last[i])
+                    $($a).append('<li class="more-info">' + order.name.last + ', ' + order.name.first +'</li>');
+                }
+                else {
+                    console.log('try again')
+                    // $('#a').text('Sorry no one')
+                }
             });
 
             // $(document).on('click', '.more-info', data.results, function (i, order) {
@@ -51,31 +54,4 @@ $(document).on('click', "nav li.active", function () {
         },
     });
 }); 
-
-//function displayNames(data) {
-//             var letterHTML = '<ul>';
-//             $.each(data.items, function (i, photo) {
-//                 letterHTML += '<li id="a">';
-//                 letterHTML += name.first + ' ' + name.last;
-//             });
-//             letterHTML += '</ul>'
-//             $('.content').append(letterHTML);
-//             console.log(data)
-//         }
-
-
-var configJson = {
-    "title": "Contact List", 
-    "uri":"https://api.randomuser.me/",
-    "numberCards": 120,
-    "tabs": ["A", "B", "c", "d", "e", "f", "g","h",
-    "i", "j", "k","l","m","n","o","p","q","r","s","t",
-    "u","v","w","x","y","z"]
-}
-
-var dataResultsName= { 
-
-}
-
-
 
