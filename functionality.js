b77b1cc26fa9646227a92df19f0d52944c51da0a
Preeti <singhp1992@gmail.com>
2019-06-17 13:35:20 +0200

    combining code - creating tabs with hiding elements

--- functionality.js
+++ functionality.js
@@ -1,4 +1,4 @@
-// creating ul tags
+// creating ul tags and API call when window loads
 $(window).load(function () {
     var ul = {
         "2": "b", "3": "c", "4": "d", "5": "e", "6": "f",
@@ -34,33 +34,6 @@ $(window).load(function () {
     });
 });
 
-//API call when window loads
-
-// $(window).load(function () {
-//     $.ajax({
-//         url: 'https://api.randomuser.me/?inc=gender,name,nat,email&results=200&nat=us,dk,fr,gb,es,nz',
-//         dataType: 'json',
-//         success: function (data) {
-//             console.log(data)
-//             $.each(data.results, function (i, order) {
-//                 $(document).ready(function () {
-//                     var tabs = {
-//                         "1": "a", "2": "b", "3": "c", "4": "d", "5": "e", "6": "f",
-//                         "7": "g", "8": "h", "9": "i", "10": "j", "11": "k", "12": "l", "13": "m", "14": "n",
-//                         "15": "o", "16": "p", "17": "q", "18": "r", "19": "s", "20": "t", "21": "u", "22": "v",
-//                         "23": "w", "24": "x", "25": "y", "26": "z"
-//                     };
-//                     $.each(tabs, function (key, value) {
-//                         if (value === order.name.last[0]) {
-//                             $('#' + value).append('<li id="all-' + value + '"' + '>' + order.name.last + ', ' + order.name.first + '</li>');
-//                         }
-//                     });
-//                 });
-//             });
-//         },
-//     });
-// });
-
 //hiding other elements when tabs are clicked
 $(document).ready(function () {
     var tabs = {
@@ -69,6 +42,11 @@ $(document).ready(function () {
         "15": "o", "16": "p", "17": "q", "18": "r", "19": "s", "20": "t", "21": "u", "22": "v",
         "23": "w", "24": "x", "25": "y", "26": "z"
     };
+
+    $.each(tabs, function (key, value) {
+        $("#result").append('<li>' + value + '</li>');
+    });
+    
     $.each(tabs, function (key, value) {
         $(document).on('click', '#' + key, function () {
             $('#' + value).removeAttr('class', 'hide').siblings().attr('class', 'hide');
@@ -77,16 +55,17 @@ $(document).ready(function () {
 });
 
 // creating tabs
-$(document).ready(function () {
-    var tabs = { "1": "a", "2": "b", "3": "c", "4": "d", "5": "e", "6": "f",
-        "7": "g", "8": "h", "9": "i", "10": "j", "11": "k", "12": "l", "13": "m", "14": "n",
-        "15": "o", "16": "p", "17": "q", "18": "r", "19": "s", "20": "t", "21": "u", "22": "v",
-        "23": "w", "24": "x", "25": "y", "26": "z"
-    };
-    $.each(tabs, function (key, value) {
-        $("#result").append('<li>' + value + '</li>');
-    });
-});
+// $(document).ready(function () {
+//     var tabs = { 
+//         "1": "a", "2": "b", "3": "c", "4": "d", "5": "e", "6": "f",
+//         "7": "g", "8": "h", "9": "i", "10": "j", "11": "k", "12": "l", "13": "m", "14": "n",
+//         "15": "o", "16": "p", "17": "q", "18": "r", "19": "s", "20": "t", "21": "u", "22": "v",
+//         "23": "w", "24": "x", "25": "y", "26": "z"
+//     };
+//     $.each(tabs, function (key, value) {
+//         $("#result").append('<li>' + value + '</li>');
+//     });
+// });
 
 // Adding ids to every <li> tags 
 $(document).ready(function() {
