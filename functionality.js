28fdfaf42cb2f29a4032053f3836407f29119ee5
Preeti <singhp1992@gmail.com>
2019-06-17 16:06:32 +0200

    cleaning up code

--- functionality.js
+++ functionality.js
@@ -94,36 +94,3 @@ $(document).on('click', 'div ul li', function () {
     $('#info').removeAttr('class', 'hide')
     $('#info').addClass('cards')
 })
-
-//when 
-    // $(".cards").append('<img src="https://randomuser.me/api/portraits/women/85.jpg">');
-    // var cardInfo = ['E-mail', 'Phone', 'Street', 'City', 'State', 'Postcode']
-    // $.each(cardInfo, function (i) {
-    //     $(".cards").append('<p class="underline" id=' + '"' + cardInfo[i] + '"' + ">" + cardInfo[i] + ": </ul>");
-    // });
-
-
-
-
-// $.ajax({
-//     url: 'https://api.randomuser.me/?inc=gender,name,nat,email&results=1&nat=us,dk,fr,gb,es,nz',
-//     dataType: 'json',
-//     success: function (data) {
-//         console.log(data)
-        // $.each(data.results, function (i, order) {
-        //     $(document).ready(function () {
-        //         var tabs = {
-        //             "1": "a", "2": "b", "3": "c", "4": "d", "5": "e", "6": "f",
-        //             "7": "g", "8": "h", "9": "i", "10": "j", "11": "k", "12": "l", "13": "m", "14": "n",
-        //             "15": "o", "16": "p", "17": "q", "18": "r", "19": "s", "20": "t", "21": "u", "22": "v",
-        //             "23": "w", "24": "x", "25": "y", "26": "z"
-        //         };
-        //         $.each(tabs, function (key, value) {
-        //             if (value === order.name.last[0]) {
-        //                 $('#' + value).append('<li id="all-' + value + '"' + '>' + order.name.last + ', ' + order.name.first + '</li>');
-        //             }
-        //         });
-        //     });
-        // });
-//     },
-// });
