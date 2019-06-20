2a4d072862b9edb4f2b4e7a8ef3f64ecb10697c2
Preeti <singhp1992@gmail.com>
2019-06-20 18:05:19 +0200

    deleted unused file

--- functionality.js
+++ functionality.js
@@ -17,28 +17,27 @@ $(window).load(function () {
         $(".cards").append('<p class="underline" id=' + '"' + cardInfo[i] + '"' + ">" + cardInfo[i] + ": </ul>");
     });
 
-    $.ajax({
-        url: 'https://api.randomuser.me/?inc=gender,name,nat,email&results=200&nat=us,',
-        dataType: 'json',
-        success: function (data) {
-            console.log(data)
-            $.each(data.results, function (i, order) {
-                $(document).ready(function () {
-                    var tabs = {
-                        "1": "a", "2": "b", "3": "c", "4": "d", "5": "e", "6": "f",
-                        "7": "g", "8": "h", "9": "i", "10": "j", "11": "k", "12": "l", "13": "m", "14": "n",
-                        "15": "o", "16": "p", "17": "q", "18": "r", "19": "s", "20": "t", "21": "u", "22": "v",
-                        "23": "w", "24": "x", "25": "y", "26": "z"
-                    };
-                    $.each(tabs, function (key, value) {
-                        if (value === order.name.last[0]) {
-                            $('#' + value).append('<li id="all-' + value + '"' + '>' + order.name.last + ', ' + order.name.first + '</li>');
-                        }
-                    });
-                });
-            });
-        },
-    });
+    //  $.ajax({
+    //     url: 'https://api.randomuser.me/?inc=gender,name,nat,email&results=200&nat=us,',
+    //     dataType: 'json',
+    //     success: function (data) {
+            // $.each(data.results, function (i, order) {
+            //     $(document).ready(function () {
+            //         var tabs = {
+            //             "1": "a", "2": "b", "3": "c", "4": "d", "5": "e", "6": "f",
+            //             "7": "g", "8": "h", "9": "i", "10": "j", "11": "k", "12": "l", "13": "m", "14": "n",
+            //             "15": "o", "16": "p", "17": "q", "18": "r", "19": "s", "20": "t", "21": "u", "22": "v",
+            //             "23": "w", "24": "x", "25": "y", "26": "z"
+            //         };
+            //         $.each(tabs, function (key, value) {
+            //             if (value === order.name.last[0]) {
+            //                 $('#' + value).append('<li id="all-' + value + '"' + '>' + order.name.last + ', ' + order.name.first + '</li>');
+            //             }
+            //         });
+            //     });
+            // });
+        // },
+    // });
 });
 
 //hiding other elements when tabs are clicked
